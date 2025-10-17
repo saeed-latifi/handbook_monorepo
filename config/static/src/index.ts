dada5b1ffd9e75ef4a0d5e2e7d37e01edce8ba35
saeed latifi <saidlatifizazerani@gmail.com>
2025-10-17 13:27:53 +0330

    add : rustFS

--- config/static/src/index.ts
+++ config/static/src/index.ts
@@ -1,5 +1,8 @@
-export const dbURL = process.env.DATABASE_URL;
-export const storageEndPoint = process.env.STORAGE_END_POINT;
-export const storagePort = process.env.STORAGE_PORT;
-export const storageAccessKey = process.env.STORAGE_ACCESS_KEY;
-export const storageSecretKey = process.env.STORAGE_SECRET_KEY;
+// db
+export const dbURL = process.env.DATABASE_URL ?? "";
+
+// storage
+export const storageEndPoint = process.env.STORAGE_END_POINT ?? "";
+export const storageRegion = process.env.STORAGE_REGION ?? "";
+export const storageAccessKey = process.env.STORAGE_ACCESS_KEY ?? "";
+export const storageSecretKey = process.env.STORAGE_SECRET_KEY ?? "";
